a403178f50371462093d8ffc81e3ed3832fcef45
Laurent Zhang <laurentz@arcology.network>
2025-04-27 14:26:23 -0600

    Enhance conflict output by adding error information to Print method in Conflict struct

--- arbitrator/conflict_info.go
+++ arbitrator/conflict_info.go
@@ -46,6 +46,7 @@ func (this *Conflict) Print() {
 	this.selfTran.Print()
 	fmt.Println(" ----- conflict with ----- ")
 	univalue.Univalues(this.conflictTrans).Print()
+	fmt.Println("Season: ", this.Err)
 }
 
 type Conflicts []*Conflict
